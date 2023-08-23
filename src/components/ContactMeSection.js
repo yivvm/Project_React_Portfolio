b0bf816bdba3d16bc5beae3e512148239709cfea
yivvm <134717898+yivvm@users.noreply.github.com>
2023-08-23 12:03:05 -0400

    Add files via upload

---
+++ src/components/ContactMeSection.js
@@ -0,0 +1,130 @@
+import React, {useEffect} from "react";
+import { useFormik } from "formik";
+import {
+  Box,
+  Button,
+  FormControl,
+  FormErrorMessage,
+  FormLabel,
+  Heading,
+  Input,
+  Select,
+  Textarea,
+  VStack,
+} from "@chakra-ui/react";
+import * as Yup from 'yup';
+import FullScreenSection from "./FullScreenSection";
+import useSubmit from "../hooks/useSubmit";
+import {useAlertContext} from "../context/alertContext";
+
+const ContactMeSection = () => {  // ori: LandingSection
+  const {isLoading, response, submit} = useSubmit();
+  const { onOpen } = useAlertContext();
+
+  const formik = useFormik({
+    // Form validation
+    initialValues: {
+      firstName: "",
+      email: "",
+      type: "",
+      comment: "",
+    },
+    onSubmit: (values) => {
+      submit("", values);
+    },
+    validationSchema: Yup.object({
+      firstName: Yup.string().required("Required"),
+      email: Yup.string().email("Invalud email address").required("Requied"),
+      // type: 
+      comment: Yup.string().min(25, "Must be at least 25 characters").required("Required")
+    }),
+  });
+
+  // show an alert when the form is submitted successfully
+  useEffect(() => {
+    if (response) {
+      onOpen(response.type, response.message);
+      // Reset the form if the response is successful
+      if (response.type === "success")
+        formik.resetForm();
+    }
+  }, [response])
+
+  return (
+    <FullScreenSection
+      isDarkBackground
+      backgroundColor="#512DA8"  // ... change color; ori: #512DA8; 
+      py={16}
+      spacing={8}
+    >
+      <VStack w="1024px" p={32} alignItems="flex-start">
+        <Heading as="h1" id="contactme-section">
+          Contact me
+        </Heading>
+        <Box p={6} rounded="md" w="100%">
+          {/* connect the form onSubmit prop with Formik's handleSubmit function */}
+          <form onSubmit={formik.handleSubmit}>
+            <VStack spacing={4}>
+              {/* show the error messages for the firstName field when the field is touched and the validation fails. */}
+              <FormControl isInvalid={!!formik.errors.firstName && formik.touched.firstName}>
+                <FormLabel htmlFor="firstName">Name</FormLabel>
+                <Input
+                  id="firstName"
+                  name="firstName"
+                  {...formik.getFieldProps("firstName")}  // make the input components from Chakra UI controlled components.
+                />
+                <FormErrorMessage>{formik.errors.firstName}</FormErrorMessage>
+              </FormControl>
+
+              {/* show the error message for the email field when the field is touched and the validation fails. */}
+              <FormControl isInvalid={!!formik.errors.email && formik.touched.email}>
+                <FormLabel htmlFor="email">Email Address</FormLabel>
+                <Input
+                  id="email"
+                  name="email"
+                  type="email"
+                  {...formik.getFieldProps("email")}  // make the input components from Chakra UI controlled components. 
+                />
+                <FormErrorMessage>{formik.errors.email}</FormErrorMessage>
+              </FormControl>
+
+              <FormControl>
+                <FormLabel htmlFor="type">Type of enquiry</FormLabel>
+                <Select 
+                  id="type" 
+                  name="type"
+                  {...formik.getFieldProps("type")}  // make the input components from Chakra UI controlled components. 
+                >
+                  <option value="hireMe">Freelance project proposal</option>
+                  <option value="openSource">
+                    Open source consultancy session
+                  </option>
+                  <option value="other">Other</option>
+                </Select>
+              </FormControl>
+
+              {/* show the error messages for the comment field when the field is touched and the validation fails. */}
+              <FormControl isInvalid={!!formik.errors.comment && formik.touched.comment}>
+                <FormLabel htmlFor="comment">Your message</FormLabel>
+                <Textarea
+                  id="comment"
+                  name="comment"
+                  height={250}
+                  {...formik.getFieldProps("comment")}
+                />
+                <FormErrorMessage></FormErrorMessage>
+              </FormControl>
+
+              {/* show a loading indicator */}
+              <Button type="submit" colorScheme="purple" width="full">
+                Submit
+              </Button>
+            </VStack>
+          </form>
+        </Box>
+      </VStack>
+    </FullScreenSection>
+  );
+};
+
+export default ContactMeSection;  // ori: LandingSection
